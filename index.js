c478da973f9abb4f05fdb2ca27edd585e10eca4a
emmariescurrena <125983228+emmariescurrena@users.noreply.github.com>
2024-08-11 15:46:51 -0300

    Reads and returns file info using express-fileupload

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
-
+const fileUpload = require('express-fileupload');
 var app = express();
+
+app.use(fileUpload());
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+    res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.use(express.urlencoded({ extended: true }));
 
+app.post('/api/fileanalyse', (req, res) => {
+    const file = req.files.upfile;
+    res.json({
+        name: file.name,
+        type: file.mimetype,
+        size: file.size,
+    });
+})
 
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+    console.log('Your app is listening on port ' + port)
 });
